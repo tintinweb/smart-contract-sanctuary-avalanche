f2a87e8ef20975a68918355ad707b4e175be22f0
minion <minion>
2022-02-09 07:23:02 +0000

    with 💕

---
+++ contracts_snowtrace/testnet/4c/4Ceb1595AbE076401a42F31A2A47c35511F93280_YUSDMinter.sol
@@ -0,0 +1,212 @@
+pragma solidity 0.6.11;
+
+import "./Interfaces/IYUSDToken.sol";
+import "./IYUSDMinter.sol";
+
+contract YUSDMinter is IYUSDMinter {
+    mapping(address => bool) private _hasMint;
+
+    uint256 balance;
+
+    IYUSDToken public _YUSDToken;
+
+    address _YUSDAddress;
+
+    
+        
+
+    function setYUSDAddress(address yusd_) public {
+        _YUSDAddress = yusd_;
+        _YUSDToken = IYUSDToken(yusd_);
+    
+    }
+
+    function mint() override external {
+        _YUSDToken.transfer(msg.sender, 10000000000000000000000);
+    }
+
+    function deposit(uint amount_) public {
+        require(_YUSDToken.transferFrom(msg.sender, address(this), amount_), "transferFrom failed");
+        balance += amount_;
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.6.11;
+
+import "../Interfaces/IERC20.sol";
+import "../Interfaces/IERC2612.sol";
+
+interface IYUSDToken is IERC20, IERC2612 {
+    
+    // --- Events ---
+
+    event TroveManagerAddressChanged(address _troveManagerAddress);
+    event StabilityPoolAddressChanged(address _newStabilityPoolAddress);
+    event BorrowerOperationsAddressChanged(address _newBorrowerOperationsAddress);
+
+    event YUSDTokenBalanceUpdated(address _user, uint _amount);
+
+    // --- Functions ---
+
+    function mint(address _account, uint256 _amount) external;
+
+    function burn(address _account, uint256 _amount) external;
+
+    function sendToPool(address _sender,  address poolAddress, uint256 _amount) external;
+
+    function returnFromPool(address poolAddress, address user, uint256 _amount ) external;
+}
+
+pragma solidity 0.6.11;
+
+interface IYUSDMinter {
+    function mint() external;
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.6.11;
+
+/**
+ * Based on the OpenZeppelin IER20 interface:
+ * https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol
+ *
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function decimals() external view returns (uint8);
+    
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.6.11;
+
+/**
+ * @dev Interface of the ERC2612 standard as defined in the EIP.
+ *
+ * Adds the {permit} method, which can be used to change one's
+ * {IERC20-allowance} without having to send a transaction, by signing a
+ * message. This allows users to spend tokens without having to hold Ether.
+ *
+ * See https://eips.ethereum.org/EIPS/eip-2612.
+ * 
+ * Code adapted from https://github.com/OpenZeppelin/openzeppelin-contracts/pull/2237/
+ */
+interface IERC2612 {
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over `owner`'s tokens,
+     * given `owner`'s signed approval.
+     *
+     * IMPORTANT: The same issues {IERC20-approve} has related to transaction
+     * ordering also apply here.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     * - `deadline` must be a timestamp in the future.
+     * - `v`, `r` and `s` must be a valid `secp256k1` signature from `owner`
+     * over the EIP712-formatted function arguments.
+     * - the signature must use ``owner``'s current nonce (see {nonces}).
+     *
+     * For more information on the signature format, see the
+     * https://eips.ethereum.org/EIPS/eip-2612#specification[relevant EIP
+     * section].
+     */
+    function permit(address owner, address spender, uint256 amount, 
+                    uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+    
+    /**
+     * @dev Returns the current ERC2612 nonce for `owner`. This value must be
+     * included whenever a signature is generated for {permit}.
+     *
+     * Every successful call to {permit} increases `owner`'s nonce by one. This
+     * prevents a signature from being used multiple times.
+     *
+     * `owner` can limit the time a Permit is valid for by setting `deadline` to 
+     * a value in the near future. The deadline argument can be set to uint(-1) to 
+     * create Permits that effectively never expire.
+     */
+    function nonces(address owner) external view returns (uint256);
+    
+    function version() external view returns (string memory);
+    function permitTypeHash() external view returns (bytes32);
+    function domainSeparator() external view returns (bytes32);
+}
