a357eb89cdfb6feac1f3bb6e1cee479acf2d21e3
minion <minion>
2022-03-08 21:10:38 +0000

    with 💕

---
+++ contracts/mainnet/75/750CcA385eF810342A8AA0EA4f37f0c2096559C9_CollateralAssetManager.sol
@@ -0,0 +1,623 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.1;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./interfaces/IDealManager.sol";
+
+interface IWNative {
+    function deposit() external payable;
+
+    function withdraw(uint256 amount) external;
+}
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+}
+
+interface IUniRouter {
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETH(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function getAmountsOut(uint256 amountIn, address[] calldata path) external view returns (uint256[] memory amounts);
+}
+
+interface IOracle {
+    function consult(address _token, uint256 _amountIn) external view returns (uint144 amountOut);
+
+    function twap(address _token, uint256 _amountIn) external view returns (uint144 _amountOut);
+}
+
+contract CollateralAssetManager is Ownable, ReentrancyGuard {
+    using SafeMath for uint256;
+
+    bytes32 public constant ASSET_TYPE = keccak256("ASSET_TYPE");
+    bytes32 public constant COLLATERAL_TYPE = keccak256("COLLATERAL_TYPE");
+
+    address public lpToken0;
+    address public lpToken1;
+    address[] public nativeToTokenRoute;
+    address public router;
+
+    IDealManager public immutable dealManager;
+
+    uint256 public price;
+
+    IOracle public oracle;
+
+    address public native;
+
+    address public feeCollector;
+
+    uint256 public decimals = 10**18;
+
+    event FeeCollected(uint256 _amount);
+    event CollateralsAdded(address[] _addresses);
+    event AssetsAdded(address[] _addresses);
+
+    constructor(
+        address _dealManager,
+        address _feeCollector,
+        uint256 _price,
+        address _oracle,
+        address _native,
+        address _router,
+        address[] memory _nativeToTokenRoute
+    ) {
+        dealManager = IDealManager(_dealManager);
+        feeCollector = _feeCollector;
+        price = _price;
+        oracle = IOracle(_oracle);
+        native = _native;
+        router = _router;
+        nativeToTokenRoute = _nativeToTokenRoute;
+    }
+
+    function makePayment(bytes32 _type, address _address) external payable nonReentrant {
+        require(_type == ASSET_TYPE || _type == COLLATERAL_TYPE, "!type");
+        require(msg.value >= getPriceNative(), "!fees");
+        address[] memory _formattedAddress = new address[](1);
+        _formattedAddress[0] = _address;
+        _addLiquidity();
+        if (_type == ASSET_TYPE) {
+            dealManager.addAsset(_formattedAddress);
+
+            emit AssetsAdded(_formattedAddress);
+        }
+        if (_type == COLLATERAL_TYPE) {
+            dealManager.addCollateral(_formattedAddress);
+
+            emit CollateralsAdded(_formattedAddress);
+        }
+
+        emit FeeCollected(price);
+    }
+
+    function getPriceNative() public view returns (uint256) {
+        return price.mul(10**decimals).mul(1e18).div(getOraclePrice());
+    }
+
+    function getOraclePrice() public view returns (uint256) {
+        try IOracle(oracle).consult(native, 1e18) returns (uint144 price) {
+            return uint256(price);
+        } catch {
+            revert("!oracle");
+        }
+    }
+
+    function getOracleUpdatedPrice() public view returns (uint256) {
+        try IOracle(oracle).twap(native, 1e18) returns (uint144 price) {
+            return uint256(price);
+        } catch {
+            revert("!oracle");
+        }
+    }
+
+    function _addLiquidity() internal {
+        IWNative(native).deposit{value: address(this).balance}();
+        IERC20 _nativeToken = IERC20(native);
+        uint256 _nativeAmount = _nativeToken.balanceOf(address(this));
+
+        IERC20 _outputToken = IERC20(nativeToTokenRoute[nativeToTokenRoute.length - 1]);
+
+        _nativeToken.approve(router, 0);
+        _nativeToken.approve(router, _nativeAmount);
+        IUniRouter(router).swapExactTokensForTokens(_nativeAmount.div(2), 0, nativeToTokenRoute, address(this), block.timestamp);
+
+        uint256 _tokenAmount = _outputToken.balanceOf(address(this));
+        _outputToken.approve(router, 0);
+        _outputToken.approve(router, _tokenAmount);
+
+        IUniRouter(router).addLiquidity(
+            native,
+            address(_outputToken),
+            _nativeToken.balanceOf(address(this)),
+            _tokenAmount,
+            0,
+            0,
+            address(feeCollector),
+            block.timestamp
+        );
+    }
+
+    function setPrice(uint256 _amount) external onlyOwner nonReentrant {
+        price = _amount;
+    }
+
+    function setFeeCollector(address _feeCollector) external onlyOwner nonReentrant {
+        feeCollector = _feeCollector;
+    }
+
+    function setDecimals(uint256 _decimals) external onlyOwner nonReentrant {
+        decimals = _decimals;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is no longer needed starting with Solidity 0.8. The compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+pragma solidity >=0.8.1;
+
+interface IDealManager {
+    function addCollateral(address[] calldata _collaterals) external;
+
+    function addAsset(address[] calldata _assets) external;
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
---
+++ contracts/mainnet/7d/7d5B7416dc01a2c54031dc2a08206FE58423FDeD_WrathCleansingEvent.sol
@@ -0,0 +1,220 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-03-08
+*/
+
+///@author Asgard Game Fi
+///@notice Special Wrath Cleansing event for Midgardian Mint
+// SPDX-License-Identifier: GPL-2.0 AND MIT
+
+pragma solidity 0.8.12;
+
+library LowGasSafeMath {
+  /// @notice Returns x + y, reverts if sum overflows uint256
+  /// @param x The augend
+  /// @param y The addend
+  /// @return z The sum of x and y
+  function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    require((z = x + y) >= x);
+  }
+
+  /// @notice Returns x - y, reverts if underflows
+  /// @param x The minuend
+  /// @param y The subtrahend
+  /// @return z The difference of x and y
+  function sub(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    require((z = x - y) <= x);
+  }
+
+  /// @notice Returns x * y, reverts if overflows
+  /// @param x The multiplicand
+  /// @param y The multiplier
+  /// @return z The product of x and y
+  function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    require(x == 0 || (z = x * y) / x == y);
+  }
+
+  /// @notice Returns x + y, reverts if overflows or underflows
+  /// @param x The augend
+  /// @param y The addend
+  /// @return z The sum of x and y
+  function add(int256 x, int256 y) internal pure returns (int256 z) {
+    require((z = x + y) >= x == (y >= 0));
+  }
+
+  /// @notice Returns x - y, reverts if overflows or underflows
+  /// @param x The minuend
+  /// @param y The subtrahend
+  /// @return z The difference of x and y
+  function sub(int256 x, int256 y) internal pure returns (int256 z) {
+    require((z = x - y) <= x == (y >= 0));
+  }
+}
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+  /**
+   * @dev Returns the amount of tokens in existence.
+   */
+  function totalSupply() external view returns (uint256);
+
+  /**
+   * @dev Returns the amount of tokens owned by `account`.
+   */
+  function balanceOf(address account) external view returns (uint256);
+
+  /**
+   * @dev Moves `amount` tokens from the caller's account to `recipient`.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Returns the remaining number of tokens that `spender` will be
+   * allowed to spend on behalf of `owner` through {transferFrom}. This is
+   * zero by default.
+   *
+   * This value changes when {approve} or {transferFrom} are called.
+   */
+  function allowance(address owner, address spender)
+    external
+    view
+    returns (uint256);
+
+  /**
+   * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * IMPORTANT: Beware that changing an allowance with this method brings the risk
+   * that someone may use both the old and the new allowance by unfortunate
+   * transaction ordering. One possible solution to mitigate this race
+   * condition is to first reduce the spender's allowance to 0 and set the
+   * desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * Emits an {Approval} event.
+   */
+  function approve(address spender, uint256 amount) external returns (bool);
+
+  /**
+   * @dev Moves `amount` tokens from `sender` to `recipient` using the
+   * allowance mechanism. `amount` is then deducted from the caller's
+   * allowance.
+   *
+   * Returns a boolean value indicating whether the operation succeeded.
+   *
+   * Emits a {Transfer} event.
+   */
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+
+  /**
+   * @dev Emitted when `value` tokens are moved from one account (`from`) to
+   * another (`to`).
+   *
+   * Note that `value` may be zero.
+   */
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  /**
+   * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+   * a call to {approve}. `value` is the new allowance.
+   */
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+interface IOwnable {
+  function owner() external view returns (address);
+
+  function renounceOwnership() external;
+
+  function transferOwnership(address newOwner_) external;
+}
+
+contract Ownable is IOwnable {
+  address internal _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  constructor() {
+    _owner = msg.sender;
+    emit OwnershipTransferred(address(0), _owner);
+  }
+
+  function owner() public view override returns (address) {
+    return _owner;
+  }
+
+  modifier onlyOwner() {
+    require(_owner == msg.sender, "Ownable: caller is not the owner");
+    _;
+  }
+
+  function renounceOwnership() public virtual override onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  function transferOwnership(address newOwner_)
+    public
+    virtual
+    override
+    onlyOwner
+  {
+    require(newOwner_ != address(0), "Ownable: new owner is the zero address");
+    emit OwnershipTransferred(_owner, newOwner_);
+    _owner = newOwner_;
+  }
+}
+
+interface WRTHERC20 is IERC20 {
+  function burnFrom(address account_, uint256 amount_) external;
+}
+
+contract WrathCleansingEvent is Ownable {
+  using LowGasSafeMath for uint256;
+
+  bool public enabled = false;
+
+  WRTHERC20 public wrth;
+  IERC20 public dai;
+
+  mapping(address => bool) public hasCleansed;
+
+  constructor(address Dai, address Wrth) {
+    require(Dai != address(0), "Zero address detected");
+    wrth = WRTHERC20(Wrth);
+    require(Wrth != address(0), "Zero address detected");
+    dai = IERC20(Dai);
+  }
+
+  function cleanseWrath() external {
+    require(!hasCleansed[msg.sender], "You have already been cleansed");
+    require(enabled, "Cleansing is disabled");
+
+    if (wrth.balanceOf(msg.sender) < 20 * 1e9) {
+      wrth.burnFrom(msg.sender, wrth.balanceOf(msg.sender));
+    } else {
+      wrth.burnFrom(msg.sender, 20 * 1e9);
+    }
+
+    dai.transferFrom(msg.sender, _owner, 50 * 1e18);
+
+    hasCleansed[msg.sender] = true;
+  }
+
+  function toggleCleanse() external onlyOwner {
+    enabled = !enabled;
+  }
+}
