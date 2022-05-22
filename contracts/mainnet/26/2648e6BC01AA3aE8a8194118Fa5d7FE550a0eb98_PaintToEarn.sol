790938c423177151256f724fa44bdcefb94e402f
minion <minion>
2022-05-22 21:12:16 +0000

    with 💕

---
+++ contracts/mainnet/26/2648e6BC01AA3aE8a8194118Fa5d7FE550a0eb98_PaintToEarn.sol
@@ -0,0 +1,253 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "./Ownable.sol";
+
+// @creator: Paint To Earn - painttoearn.io
+
+contract PaintToEarn is Ownable {
+    
+    bool paintState = true;
+    uint256 paintPrice = 0.0027 ether;
+
+    uint256 canvasSizeWidth = 1000;
+    uint256 canvasSizeHeight = 1000;
+    
+    mapping(uint256 => string) colorPalette;
+    mapping(address => uint256) addressPaintCount;
+
+    mapping(uint256 => mapping(uint256 => address)) coordinateAddress;
+    mapping(uint256 => mapping(uint256 => string)) coordinateColor;
+
+    address founder1 = 0x524192ce030D38aD15cfa1b98F19A55AF692feBD;
+    address founder2 = 0xBff3A6caec37850f23Db6ebb9E23C884eCBE13BD;
+
+    constructor() {
+        colorPalette[0] = '#FFD635'; // Yellow
+        colorPalette[1] = '#FFA800'; // Light Orange
+        colorPalette[2] = '#FF4500'; // Orange
+        colorPalette[3] = '#BE0039'; // Red
+        colorPalette[4] = '#B44AC0'; // Light Purple
+        colorPalette[5] = '#811E9F'; // Purple
+        colorPalette[6] = '#493AC1'; // Dark Blue
+        colorPalette[7] = '#3690EA'; // Blue
+        colorPalette[8] = '#00A368'; // Green
+        colorPalette[9] = '#00CC78'; // Light Green
+        colorPalette[10] = '#FFFFFF'; // White
+        colorPalette[11] = '#D4D7D9'; // Light Gray
+        colorPalette[12] = '#898D90'; // Gray
+        colorPalette[13] = '#000000'; // Black
+    }
+
+    function clearCoordinate(uint256 x, uint256 y) public onlyOwner {
+        require(x >= 0 && x < canvasSizeWidth && y >= 0 && y < canvasSizeHeight, "Coordinate doesn't exist.");
+        coordinateAddress[x][y] = 0x0000000000000000000000000000000000000000;
+        coordinateColor[x][y] = '#000000';
+    }
+
+    function ownerPaintCoordinate(uint256 x, uint256 y, address _address, string memory color) public onlyOwner {
+        require(x >= 0 && x < canvasSizeWidth && y >= 0 && y < canvasSizeHeight, "Coordinate doesn't exist.");
+        
+        require(checkColorInColorPalette(color) == true, "Color is not in the color palette.");
+
+        addressPaintCount[msg.sender]++;
+        coordinateAddress[x][y] = _address;
+        coordinateColor[x][y] = color;
+    }
+
+    function paintCoordinate(uint256 x, uint256 y, address _address, string calldata color) public payable {
+        require(paintState == true, "Painting is not active now.");
+        
+        require(x >= 0 && x < canvasSizeWidth && y >= 0 && y < canvasSizeHeight, "Coordinate doesn't exist.");
+
+        require(msg.value >= paintPrice, "Insufficient funds.");
+
+        require(checkColorInColorPalette(color) == true, "Color is not in the color palette.");
+        require(checkCoordinateAddressAndColor(x, y, _address, color) == false, "Same address already painted same color into this coordinate.");
+
+        addressPaintCount[msg.sender]++;
+        coordinateAddress[x][y] = _address;
+        coordinateColor[x][y] = color;
+    }
+
+    function checkColorInColorPalette(string memory color) public view returns (bool) {
+        for(uint256 i=0; i < 14; i++) {
+            if (keccak256(abi.encodePacked(colorPalette[i])) == keccak256(abi.encodePacked(color))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function checkCoordinateAddressAndColor(uint256 x, uint256 y, address _address, string memory color) internal view returns (bool) {
+        if (keccak256(abi.encodePacked(coordinateAddress[x][y])) == keccak256(abi.encodePacked(_address)) && keccak256(abi.encodePacked(coordinateColor[x][y])) == keccak256(abi.encodePacked(color))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function getCoordinateAddress(uint256 x, uint256 y) public view returns (address) {
+        require(x >= 0 && x < canvasSizeWidth && y >= 0 && y < canvasSizeHeight, "Coordinate doesn't exist.");
+        return coordinateAddress[x][y];
+    }
+
+    function getCoordinateColor(uint256 x, uint256 y) public view returns (string memory) {
+        require(x >= 0 && x < canvasSizeWidth && y >= 0 && y < canvasSizeHeight, "Coordinate doesn't exist.");
+
+        if (keccak256(abi.encodePacked(coordinateColor[x][y])) == keccak256(abi.encodePacked(''))) {
+            return '#000000';
+        } else {
+            return coordinateColor[x][y];
+        }
+    }
+
+    function updatePaintPrice(uint256 price) public onlyOwner {
+        paintPrice = price;
+    }
+
+    function updatePaintState(bool state) public onlyOwner {
+        paintState = state;
+    }
+
+    function getPaintPrice() public view returns(uint256) {
+        return paintPrice;
+    }
+
+    function getPaintState() public view returns(bool) {
+        return paintState;
+    }
+
+    function getCanvasSizeWidth() public view returns(uint256) {
+        return canvasSizeWidth;
+    }
+
+    function getCanvasSizeHeight() public view returns(uint256) {
+        return canvasSizeHeight;
+    }    
+
+    function getColorPalette(uint256 index) public view returns(string memory) {
+        return colorPalette[index];
+    }
+
+    function getAddressPaintCount(address _address) public view returns(uint256) {
+        return addressPaintCount[_address];
+    }
+    
+    function withdraw() public payable onlyOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "Not enough balance.");
+
+        (bool successFounder1, ) = payable(founder1).call{value: ((balance * 500) / 1000)}("");
+        require(successFounder1, "Transfer failed.");
+
+        (bool successFounder2, ) = payable(founder2).call{value: ((balance * 500) / 1000)}("");
+        require(successFounder2, "Transfer failed.");
+
+        (bool successOwner, ) = payable(msg.sender).call{value: (address(this).balance)}("");
+        require(successOwner, "Transfer failed.");
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "./Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
---
+++ contracts/mainnet/9d/9d25575ae7e15cc40c515c28abbadf2c3f9bfed8_TestAvaxlashSwap8.sol
@@ -0,0 +1,273 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-05-22
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IPangolinERC20 {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    function name() external pure returns (string memory);
+    function symbol() external pure returns (string memory);
+    function decimals() external pure returns (uint8);
+    function totalSupply() external view returns (uint);
+    function balanceOf(address owner) external view returns (uint);
+    function allowance(address owner, address spender) external view returns (uint);
+
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+    function nonces(address owner) external view returns (uint);
+
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+}
+
+interface IUniswapV2Router {
+  function getAmountsOut(uint amountIn, address[] memory path)
+    external
+    view
+    returns (uint[] memory amounts);
+
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactTokensForAVAX(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactAVAXForTokens(
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external payable returns (uint[] memory amounts);
+
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  )
+    external
+    returns (
+      uint amountA,
+      uint amountB,
+      uint liquidity
+    );
+
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+
+  function token1() external view returns (address);
+
+  function getReserves()
+    external
+    view
+    returns (
+      uint112 reserve0,
+      uint112 reserve1,
+      uint32 blockTimestampLast
+    );
+
+  function swap(
+    uint amount0Out,
+    uint amount1Out,
+    address to,
+    bytes calldata data
+  ) external;
+}
+
+interface IUniswapV2Factory {
+  function getPair(address token0, address token1) external view returns (address);
+}
+
+interface IPangolinCallee {
+  function pangolinCall(
+    address sender,
+    uint amount0,
+    uint amount1,
+    bytes calldata data
+  ) external;
+}
+
+contract TestAvaxlashSwap8 is IPangolinCallee {
+  // Uniswap V2 router
+  // 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+  address private constant WAVAX = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+  // Uniswap V2 factory
+  address private constant FACTORY = 0xefa94DE7a4656D787667C749f7E1223D71E9FD88;
+
+  address private constant PANGOLIN_ROUTER = 0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106;
+
+  address private constant JOE_ROUTER = 0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
+
+  event Log(string message, uint val);
+  address private immutable owner;
+
+  constructor() public payable {
+    owner = msg.sender;
+  }
+
+  receive() external payable {
+  }
+
+  function getAmountOutMin(
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn,
+    address ROUTER
+    ) internal view returns (uint) {
+        address[] memory path;
+        if (_tokenIn == WAVAX || _tokenOut == WAVAX) {
+        path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;                
+        } else {
+        path = new address[](3);
+        path[0] = _tokenIn;
+        path[1] = WAVAX;
+        path[2] = _tokenOut;
+        }
+
+        uint[] memory amountOutMins = IUniswapV2Router(ROUTER).getAmountsOut(_amountIn, path);
+        return amountOutMins[path.length - 1];
+    }
+
+
+  function swap(
+    string memory _routerAddress,  
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn
+    ) internal {
+        address ROUTER;
+        if (keccak256(abi.encodePacked(_routerAddress)) == keccak256(abi.encodePacked("pangolin"))) {
+            ROUTER = PANGOLIN_ROUTER;
+        } else {
+            ROUTER = JOE_ROUTER;
+        }
+
+        uint _amountOutMin = getAmountOutMin(_tokenIn, _tokenOut, _amountIn, ROUTER);
+
+        IPangolinERC20(_tokenIn).approve(ROUTER, _amountIn);
+
+        address[] memory path;
+        if (_tokenIn == WAVAX || _tokenOut == WAVAX) {
+            path = new address[](2);
+            path[0] = _tokenIn;
+            path[1] = _tokenOut;
+        } else {
+            path = new address[](3);
+            path[0] = _tokenIn;
+            path[1] = WAVAX;
+            path[2] = _tokenOut;
+        }
+
+        IUniswapV2Router(ROUTER).swapExactTokensForTokens(
+            _amountIn,
+            _amountOutMin,
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+  modifier onlyOwner() {
+    require (msg.sender == owner);
+    _;
+    }
+  
+  function withdrawToken(address _tokenContract, uint256 _amount) external onlyOwner {
+        IPangolinERC20 tokenContract = IPangolinERC20(_tokenContract);
+        
+        // transfer the token from address of this contract
+        // to address of the user (executing the withdrawToken() function)
+        tokenContract.transfer(msg.sender, _amount);
+    }
+
+
+
+  function testFlashSwap(address _tokenBorrow, uint _amount, address _arbtoken, string memory _option) external payable {
+    address pair = IUniswapV2Factory(FACTORY).getPair(_tokenBorrow, WAVAX);
+    require(pair != address(0), "!pair");
+
+    address token0 = IUniswapV2Pair(pair).token0();
+    address token1 = IUniswapV2Pair(pair).token1();
+    uint amount0Out = _tokenBorrow == token0 ? _amount : 0;
+    uint amount1Out = _tokenBorrow == token1 ? _amount : 0;
+
+    // need to pass some data to trigger uniswapV2Call
+    bytes memory data = abi.encode(_tokenBorrow, _amount, _arbtoken, _option);
+    emit Log("Attempting swap now", 6);
+
+    IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+  }
+
+  // called by pair contract
+  function pangolinCall(
+    address _sender,
+    uint _amount0,
+    uint _amount1,
+    bytes calldata _data
+  ) external override {
+    address token0 = IUniswapV2Pair(msg.sender).token0();
+    address token1 = IUniswapV2Pair(msg.sender).token1();
+    address pair = IUniswapV2Factory(FACTORY).getPair(token0, token1);
+    require(msg.sender == pair, "!pair");
+    require(_sender == address(this), "!sender");
+
+    (address tokenBorrow, uint amount, address arbtoken, string memory option) = abi.decode(_data, (address, uint, address, string));
+
+    // about 0.3%
+    uint fee = ((amount * 3) / 997) + 1;
+    uint amountToRepay = amount + fee;
+    emit Log("Attempting swap now", 7);
+
+    if (keccak256(abi.encodePacked(option)) == keccak256(abi.encodePacked("joeFirst"))) {
+        swap("pangolin", tokenBorrow, arbtoken, amount);
+        uint amount2 = IPangolinERC20(arbtoken).balanceOf(address(this));
+        swap("joe", arbtoken, tokenBorrow, amount2); 
+    } else {
+        swap("joe", tokenBorrow, arbtoken, amount);
+        uint amount2 = IPangolinERC20(arbtoken).balanceOf(address(this));
+        swap("pangolin", arbtoken, tokenBorrow, amount2);
+    }
+
+    emit Log("amount", amount);
+    emit Log("amount0", _amount0);
+    emit Log("amount1", _amount1);
+    emit Log("fee", fee);
+    emit Log("amount to repay", amountToRepay);
+
+    IPangolinERC20(tokenBorrow).transfer(pair, amountToRepay);
+  }
+}
