8513321401cf107a93ecc905037ebbea41fccc04
minion <minion>
2022-07-08 21:19:51 +0000

    with 💕

---
+++ contracts/mainnet/fd/fd7372feb9bd69a4965197acf2476bbdca5a723a_flashSwap.sol
@@ -0,0 +1,165 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-07-08
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+
+interface IERC20 {
+    function transfer(address to, uint value) external returns (bool);
+}
+
+interface IUniswapV2Router {
+  function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+  function getAmountsOut(uint amountIn, address[] memory path)
+    external
+    view
+    returns (uint[] memory amounts);
+
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactTokensForETH(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactETHForTokens(
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external payable returns (uint[] memory amounts);
+
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  )
+    external
+    returns (
+      uint amountA,
+      uint amountB,
+      uint liquidity
+    );
+
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+}
+
+interface IUniswapV2Pair {
+  function token0() external view returns (address);
+
+  function token1() external view returns (address);
+
+  function getReserves()
+    external
+    view
+    returns (
+      uint112 reserve0,
+      uint112 reserve1,
+      uint32 blockTimestampLast
+    );
+
+  function swap(
+    uint amount0Out,
+    uint amount1Out,
+    address to,
+    bytes calldata data
+  ) external;
+}
+
+interface IUniswapV2Factory {
+  function getPair(address token0, address token1) external view returns (address);
+}
+
+interface IUniswapV2Callee {
+    function uniswapV2Call(
+        address sender,
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external;
+}
+
+
+contract flashSwap is IUniswapV2Callee {
+    address private constant WETH = 0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7;
+    address private constant UniswapV2Factory =0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10;
+    address private constant router =0x60aE616a2155Ee3d9A68541Ba4544862310933d4;
+
+
+    // we'll call this function to call to call FLASHLOAN on uniswap
+    function testFlashSwap(address _tokenBorrow, uint256 _amount) external {
+        // check the pair contract for token borrow and weth exists
+        address pair = IUniswapV2Factory(UniswapV2Factory).getPair(
+            _tokenBorrow,
+            WETH
+        );
+        require(pair != address(0), "!pair");
+
+        // right now we dont know tokenborrow belongs to which token
+        address token0 = IUniswapV2Pair(pair).token0();
+        address token1 = IUniswapV2Pair(pair).token1();
+
+        // as a result, either amount0out will be equal to 0 or amount1out will be
+        uint256 amount0Out = _tokenBorrow == token0 ? _amount : 0;
+        uint256 amount1Out = _tokenBorrow == token1 ? _amount : 0;
+
+        // need to pass some data to trigger uniswapv2call
+        bytes memory data = abi.encode(_tokenBorrow, _amount);
+        // last parameter tells whether its a normal swap or a flash swap
+        IUniswapV2Pair(pair).swap(amount0Out, amount1Out, address(this), data);
+        // adding data triggers a flashloan
+    }
+
+    // in return of flashloan call, uniswap will return with this function
+    // providing us the token borrow and the amount
+    // we also have to repay the borrowed amt plus some fees
+    function uniswapV2Call(
+        address _sender,
+        uint256 _amount0,
+        uint256 _amount1,
+        bytes calldata _data
+    ) external override {
+        // check msg.sender is the pair contract
+        // take address of token0 n token1
+        address token0 = IUniswapV2Pair(msg.sender).token0();
+        address token1 = IUniswapV2Pair(msg.sender).token1();
+        // call uniswapv2factory to getpair 
+        address pair = IUniswapV2Factory(UniswapV2Factory).getPair(token0, token1);
+        require(msg.sender == pair, "!pair");
+        // check sender holds the address who initiated the flash loans
+        require(_sender == address(this), "!sender");
+
+        (address tokenBorrow, uint amount) = abi.decode(_data, (address, uint));
+
+
+
+        uint fee = ((amount * 3) / 997) + 1;
+        uint amountToRepay = amount + fee;
+
+        IERC20(tokenBorrow).transfer(pair, amountToRepay);
+    }
+    }
