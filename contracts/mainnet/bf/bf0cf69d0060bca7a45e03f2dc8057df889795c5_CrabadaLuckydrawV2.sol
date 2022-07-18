d9d0667a902d11a60f1555ea3f99821a2977b879
minion <minion>
2022-07-18 05:34:18 +0000

    with 💕

---
+++ contracts/mainnet/bf/bf0cf69d0060bca7a45e03f2dc8057df889795c5_CrabadaLuckydrawV2.sol
@@ -0,0 +1,14 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-07-18
+*/
+
+pragma solidity 0.8.3;
+// SPDX-License-Identifier: MIT
+
+    contract CrabadaLuckydrawV2  {
+
+    function claimReward(uint256 drawId) external {}
+    function buyTicketAmulet(uint256 drawId, uint256 amountOfTicket) external {    }
+    function buyTicketTUS(uint256 drawId, uint256 amountOfTicket) external {   }
+
+    }
