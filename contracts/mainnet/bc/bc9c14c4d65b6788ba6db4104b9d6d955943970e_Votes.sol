c9534951ec63079787db8fbca9a14b193bd1c912
minion <minion>
2022-07-26 21:05:52 +0000

    with 💕

---
+++ contracts/mainnet/bc/bc9c14c4d65b6788ba6db4104b9d6d955943970e_Votes.sol
@@ -0,0 +1,10 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-07-26
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Votes {
+    function castVote(bytes32 id, bytes32 initiative, uint points, uint timestamp) public {}
+}
