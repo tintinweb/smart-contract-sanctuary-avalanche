182cb343d10eafbe4b26efd6d5aabd6c4dce5835
minion <minion>
2022-07-31 21:05:50 +0000

    with 💕

---
+++ contracts/mainnet/58/58D462b7d58B4B8D706A909C1188E9aEccd17377_SecretFilesII.sol
@@ -0,0 +1,209 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.7;
+
+/// @title IERC2981Royalties
+/// @dev Interface for the ERC2981 - Token Royalty standard
+interface IERC2981Royalties {
+    /// @notice Called with the sale price to determine how much royalty
+    //          is owed and to whom.
+    /// @param _tokenId - the NFT asset queried for royalty information
+    /// @param _value - the sale price of the NFT asset specified by _tokenId
+    /// @return _receiver - address of who should be sent the royalty payment
+    /// @return _royaltyAmount - the royalty payment amount for value sale price
+    function royaltyInfo(uint256 _tokenId, uint256 _value)
+        external
+        view
+        returns (address _receiver, uint256 _royaltyAmount);
+}
+
+
+pragma solidity ^0.8.7;
+
+import "ERC165.sol";
+
+/// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
+abstract contract ERC2981Base is ERC165, IERC2981Royalties {
+    struct RoyaltyInfo {
+        address recipient;
+        uint24 amount;
+    }
+
+    /// @inheritdoc	ERC165
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override
+        returns (bool)
+    {
+        return
+            interfaceId == type(IERC2981Royalties).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
+
+pragma solidity ^0.8.7;
+
+/// @dev This is a contract used to add ERC2981 support to ERC721 and 1155
+/// @dev This implementation has the same royalties for each and every tokens
+abstract contract ERC2981ContractWideRoyalties is ERC2981Base {
+    RoyaltyInfo private _royalties;
+
+    /// @dev Sets token royalties
+    /// @param recipient recipient of the royalties
+    /// @param value percentage (using 2 decimals - 10000 = 100, 0 = 0)
+    function _setRoyalties(address recipient, uint256 value) internal {
+        require(value <= 10000, 'ERC2981Royalties: Too high');
+        _royalties = RoyaltyInfo(recipient, uint24(value));
+    }
+
+    /// @inheritdoc	IERC2981Royalties
+    function royaltyInfo(uint256, uint256 value)
+        external
+        view
+        override
+        returns (address receiver, uint256 royaltyAmount)
+    {
+        RoyaltyInfo memory royalties = _royalties;
+        receiver = royalties.recipient;
+        royaltyAmount = (value * royalties.amount) / 10000;
+    }
+}
+
+pragma solidity >=0.8.7 <0.9.0;
+
+import "ERC721Enumerable.sol";
+import "Ownable.sol";
+import "Counters.sol";
+
+
+contract SecretFilesII is ERC721Enumerable, Ownable, ERC2981ContractWideRoyalties {
+  using Strings for uint256;
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+  mapping(address => bool) private _owners;
+  string public baseURI;
+  uint24 maxSupply = 333;
+  mapping(address => uint8) private _whiteList;
+  uint256 saleStart = 1690761600;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI
+  ) ERC721(_name, _symbol) {
+    // nft init
+    setBaseURI(_initBaseURI);
+    // set royalties
+    _setRoyalties(msg.sender, 1000);
+
+  }
+
+  /// @inheritdoc	ERC165
+  function supportsInterface(bytes4 interfaceId)
+      public
+      view
+      virtual
+      override(ERC721Enumerable, ERC2981Base)
+      returns (bool)
+  {
+      return super.supportsInterface(interfaceId);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  function totalSupply() public view virtual override returns (uint256) {
+    return _tokenIds.current();
+  }
+
+  // public
+  function mint() public notOwner(msg.sender) {
+    require(saleStart <= block.timestamp, "Sale has not started yet");
+    require(_tokenIds.current() < maxSupply , "Max supply has been reached");
+    if(block.timestamp <= saleStart + 10 minutes){
+      require(_whiteList[msg.sender] == 1, "Address not white-listed");
+    }
+    _tokenIds.increment();
+    uint256 newItemId = _tokenIds.current();
+    _owners[msg.sender] = true;
+    _safeMint(msg.sender, newItemId);
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), ".json"))
+        : "";
+  }
+
+  //only owner
+  
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function startSale() public onlyOwner {
+    saleStart = block.timestamp;
+  }
+
+  function airdrop(address[] calldata addresses) public onlyOwner {
+    uint256 newItemId = _tokenIds.current();
+    for (uint256 i = 0; i < addresses.length; i++) {
+      _tokenIds.increment();
+      newItemId = _tokenIds.current();
+      _safeMint(addresses[i],newItemId);
+    }
+  }
+
+  function withdraw() public payable onlyOwner {
+    (bool os, ) = payable(owner()).call{value: address(this).balance}("");
+    require(os);
+  }
+  
+  /// @notice Allows to set the royalties on the contract
+  /// @param recipient the royalties recipient
+  /// @param value royalties value (between 0 and 10000)
+  function setRoyalties(address recipient, uint256 value) public onlyOwner {
+      _setRoyalties(recipient, value);
+  }
+
+  function setWhiteList(address[] calldata addresses) external onlyOwner {
+    for (uint256 i = 0; i < addresses.length; i++) {
+        _whiteList[addresses[i]] = 1;
+    }
+  }
+
+  modifier notOwner(address to){
+    require(!_owners[to], "This address already minted a token");
+    _;
+  }
+  
+}
