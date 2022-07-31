4dd6c2cf70337bafec6aa0ca9546dac8e3d442c3
minion <minion>
2022-07-31 07:10:18 +0000

    with 💕

---
+++ contracts/testnet/57/577fF230C9b1c26F3810bF80752Db31eBa86f1A8_ExecutionManager.sol
@@ -0,0 +1,273 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+import {Owned} from '../utils/Owned.sol';
+
+import {IExecutionManager} from '../interfaces/IExecutionManager.sol';
+import {IForumGroupTypes} from '../interfaces/IForumGroupTypes.sol';
+import {IProposalHandler} from '../interfaces/IProposalHandler.sol';
+import {IERC20} from '../interfaces/IERC20.sol';
+
+/**
+ * @title ExecutionManager
+ * @notice It allows adding/removing proposalHandlers to collect fees from proposals.
+ * @author Modified from Looksrare (https://github.com/LooksRare/contracts-exchange-v1/blob/master/contracts/ExecutionManager.sol)
+ */
+contract ExecutionManager is IExecutionManager, IForumGroupTypes, Owned {
+	/// ----------------------------------------------------------------------------------------
+	///							ERRORS & EVENTS
+	/// ----------------------------------------------------------------------------------------
+
+	error TransferFailed();
+
+	error UnapprovedContract();
+
+	event proposalHandlerUpdated(address indexed handledAddress, address indexed newProposalHandler);
+
+	event proposalHandlerAdded(address indexed newHandledAddress, address indexed proposalHandler);
+
+	event restrictedExecutionToggled(uint256 newRestrictionSetting);
+
+	/// ----------------------------------------------------------------------------------------
+	///							ExecutionManager Storage
+	/// ----------------------------------------------------------------------------------------
+
+	/// @notice If equal to 1 then only cerin contracts can be called
+	uint256 public restrictedExecution = 1;
+
+	/// @notice Each proposalHandler is an address with logic to extract the details of the proposal to take commission fees from.
+	mapping(address => address) public proposalHandlers;
+
+	/// ----------------------------------------------------------------------------------------
+	///							Constructor
+	/// ----------------------------------------------------------------------------------------
+
+	constructor(address deployer) Owned(deployer) {}
+
+	/// ----------------------------------------------------------------------------------------
+	///							Owner Interface
+	/// ----------------------------------------------------------------------------------------
+
+	/**
+	 * @notice Add a proposalHandler
+	 * @param newHandledAddress address of contract to handle proposals for
+	 * @param proposalHandler address of the proposalHandler
+	 */
+	function addProposalHandler(address newHandledAddress, address proposalHandler)
+		external
+		onlyOwner
+	{
+		proposalHandlers[newHandledAddress] = proposalHandler;
+		emit proposalHandlerAdded(newHandledAddress, proposalHandler);
+	}
+
+	/**
+	 * @notice Update a proposalHandler
+	 * @param handledAddress address of the contract which we handle proposals for
+	 * @param newProposalHandler address of the updated handler
+	 */
+	function updateProposalHandler(address handledAddress, address newProposalHandler)
+		external
+		onlyOwner
+	{
+		proposalHandlers[handledAddress] = newProposalHandler;
+
+		emit proposalHandlerUpdated(handledAddress, newProposalHandler);
+	}
+
+	/**
+	 * @notice Change the restrictedExecution setting
+	 * @param _restrictedExecution new restricted execution setting (1 = restricted, 0 = not restricted)
+	 */
+	function setRestrictedExecution(uint256 _restrictedExecution) external onlyOwner {
+		restrictedExecution = _restrictedExecution;
+
+		emit restrictedExecutionToggled(_restrictedExecution);
+	}
+
+	/**
+	 * @notice Collect native fees
+	 * @dev This is not onlyOwner to enable automation of the fee collection.
+	 */
+	function collectFees() external {
+		(bool success, ) = payable(owner).call{value: address(this).balance}(new bytes(0));
+		if (!success) revert TransferFailed();
+	}
+
+	/**
+	 * @notice Collect token fees
+	 * @dev This is not onlyOwner to enable automation of the fee collection.
+	 */
+	function collectERC20(IERC20 erc20) external {
+		IERC20(erc20).transfer(owner, erc20.balanceOf(address(this)));
+	}
+
+	/// ----------------------------------------------------------------------------------------
+	///							Public Interface
+	/// ----------------------------------------------------------------------------------------
+
+	/**
+	 * @notice Manage the routing to a proposalHandler based on the contract
+	 * @param target target contract for proposal
+	 * @param value value of tx
+	 * @param payload payload sent to contract which will be decoded
+	 */
+	function manageExecution(
+		address target,
+		uint256 value,
+		bytes memory payload
+	) external view returns (uint256) {
+		// If the target does not already have a handler set
+		// If restriction is off process, else revert
+		if (proposalHandlers[target] == address(0))
+			if (restrictedExecution == 0) return 0;
+			else revert UnapprovedContract();
+
+		return IProposalHandler(proposalHandlers[target]).handleProposal(value, payload);
+	}
+
+	receive() external payable virtual {}
+}
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+/// @notice Simple single owner authorization mixin.
+/// @author Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/auth/Owned.sol)
+abstract contract Owned {
+	/*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+	event OwnerUpdated(address indexed user, address indexed newOwner);
+
+	/*//////////////////////////////////////////////////////////////
+                            OWNERSHIP STORAGE
+    //////////////////////////////////////////////////////////////*/
+
+	address public owner;
+
+	modifier onlyOwner() virtual {
+		require(msg.sender == owner, 'UNAUTHORIZED');
+
+		_;
+	}
+
+	/*//////////////////////////////////////////////////////////////
+                               CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+	constructor(address _owner) {
+		owner = _owner;
+
+		emit OwnerUpdated(address(0), _owner);
+	}
+
+	/*//////////////////////////////////////////////////////////////
+                             OWNERSHIP LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+	function setOwner(address newOwner) public virtual onlyOwner {
+		owner = newOwner;
+
+		emit OwnerUpdated(msg.sender, newOwner);
+	}
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+/// @notice Execution Manager interface.
+/// @author Modified from Looksrare (https://github.com/LooksRare/contracts-exchange-v1/blob/master/contracts/ExecutionManager.sol)
+
+import {IForumGroupTypes} from '../interfaces/IForumGroupTypes.sol';
+
+interface IExecutionManager {
+	function addProposalHandler(address newHandledAddress, address handlerAddress) external;
+
+	function updateProposalHandler(address proposalHandler, address newProposalHandler) external;
+
+	function manageExecution(
+		address target,
+		uint256 value,
+		bytes memory payload
+	) external returns (uint256);
+}
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+/// @notice ForumGroup interface for sharing types
+interface IForumGroupTypes {
+	enum ProposalType {
+		MINT, // add membership
+		BURN, // revoke membership
+		CALL, // call contracts
+		VPERIOD, // set `votingPeriod`
+		GPERIOD, // set `gracePeriod`
+		MEMBER_THRESHOLD, // set `memberVoteThreshold`
+		SUPERMAJORITY, // set `supermajority`
+		TYPE, // set `VoteType` to `ProposalType`
+		PAUSE, // flip membership transferability
+		EXTENSION, // flip `extensions` whitelisting
+		ESCAPE, // delete pending proposal in case of revert
+		DOCS, // amend org docs
+		DELEGATION, // enable delegation
+		PFP, // change the group pfp
+		ALLOW_CONTRACT_SIG // enable the contract to sign as an EOA
+	}
+
+	enum VoteType {
+		MEMBER,
+		SIMPLE_MAJORITY,
+		SUPERMAJORITY
+	}
+
+	/// @dev AccountMetadata is created by shifting uint16 values into a single uint256.
+	/// It stores info we use to determine commissions on trade or how to handle proposals to external contracts
+	struct Proposal {
+		ProposalType proposalType;
+		address[] accounts; // member(s) being added/kicked; account(s) receiving payload
+		uint256[] amounts; // value(s) to be minted/burned/spent; gov setting [0]
+		bytes[] payloads; // data for CALL proposals
+		uint96 yesVotes;
+		uint32 creationTime;
+		address proposer;
+	}
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.13;
+
+/// @notice ProposalHandler interface.
+
+interface IProposalHandler {
+	function handleProposal(uint256 value, bytes memory payload) external view returns (uint256);
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.13;
+
+/// @dev Interface of the ERC20 standard as defined in the EIP.
+interface IERC20 {
+	event Transfer(address indexed from, address indexed to, uint256 value);
+	event Approval(address indexed owner, address indexed spender, uint256 value);
+
+	function totalSupply() external view returns (uint256);
+
+	function balanceOf(address account) external view returns (uint256);
+
+	function transfer(address to, uint256 amount) external returns (bool);
+
+	function allowance(address owner, address spender) external view returns (uint256);
+
+	function approve(address spender, uint256 amount) external returns (bool);
+
+	function transferFrom(
+		address from,
+		address to,
+		uint256 amount
+	) external returns (bool);
+}
---
+++ contracts/testnet/d0/d05baa677CDBde12DD2661972Fa7c99e2Fe92440_ForumGroupFundraise.sol
@@ -0,0 +1,387 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.13;
+
+import {SafeTransferLib} from '../../../libraries/SafeTransferLib.sol';
+
+import {ReentrancyGuard} from '../../../utils/ReentrancyGuard.sol';
+
+import {IForumGroup} from '../../../interfaces/IForumGroup.sol';
+
+/**
+ * @title ForumGroupFundraise
+ * @notice Contract that implements a round of fundraising from all DAO members
+ * @dev Version 1 - AVAX only fundraise. All members must contribute
+ */
+contract ForumGroupFundraise is ReentrancyGuard {
+	using SafeTransferLib for address;
+
+	/// -----------------------------------------------------------------------
+	/// Events
+	/// -----------------------------------------------------------------------
+
+	event NewFundContribution(address indexed groupAddress, address indexed proposer, uint256 value);
+
+	event FundProposalCancelled(address indexed groupAddress);
+
+	event FundProposalReleased(
+		address indexed groupAddress,
+		address[] contributors,
+		uint256 individualContribution
+	);
+
+	/// -----------------------------------------------------------------------
+	/// Errors
+	/// -----------------------------------------------------------------------
+
+	error NotProposer();
+
+	error NotMember();
+
+	error MembersMissing();
+
+	error FundraiseMissing();
+
+	error IncorrectContribution();
+
+	error OpenFund();
+
+	/// -----------------------------------------------------------------------
+	/// Fundraise Storage
+	/// -----------------------------------------------------------------------
+
+	// valueNumerator and valueDenominator combine to form unitValue of treasury tokens
+	// This is used to determin how many token to mint for eac contributor
+	struct Fund {
+		address[] contributors;
+		uint256 individualContribution;
+		uint256 valueNumerator;
+		uint256 valueDenominator;
+	}
+
+	uint256 private constant MEMBERSHIP = 0;
+	uint256 private constant TOKEN = 1;
+
+	mapping(address => Fund) private funds;
+
+	mapping(address => mapping(address => bool)) public contributionTracker;
+
+	/// -----------------------------------------------------------------------
+	/// Fundraise Logic
+	/// -----------------------------------------------------------------------
+
+	/**
+	 * @notice Submit a proposal for a fundraise
+	 * @param groupAddress Address of group
+	 */
+	function submitFundProposal(
+		address groupAddress,
+		uint256 valueNumerator,
+		uint256 valueDenominator
+	) public payable virtual nonReentrant {
+		// Only members can start a fund.
+		if (IForumGroup(groupAddress).balanceOf(msg.sender, MEMBERSHIP) == 0) revert NotMember();
+
+		if (funds[groupAddress].individualContribution != 0) revert OpenFund();
+
+		// No gas saving to use Fund({}) format, and since we need to push to the arry, we assign each element individually.
+		funds[groupAddress].contributors.push(msg.sender);
+		funds[groupAddress].individualContribution = msg.value;
+		funds[groupAddress].valueNumerator = valueNumerator;
+		funds[groupAddress].valueDenominator = valueDenominator;
+		contributionTracker[groupAddress][msg.sender] = true;
+
+		emit NewFundContribution(groupAddress, msg.sender, msg.value);
+	}
+
+	/**
+	 * @notice Submit a fundraise contribution
+	 * @param groupAddress Address of group
+	 */
+	function submitFundContribution(address groupAddress) public payable virtual nonReentrant {
+		// Only members can contribute to the fund
+		if (IForumGroup(groupAddress).balanceOf(msg.sender, MEMBERSHIP) == 0) revert NotMember();
+
+		// Can only contribute once per fund
+		if (contributionTracker[groupAddress][msg.sender]) revert IncorrectContribution();
+
+		Fund storage fund = funds[groupAddress];
+
+		if (msg.value != fund.individualContribution) revert IncorrectContribution();
+
+		if (funds[groupAddress].individualContribution == 0) revert FundraiseMissing();
+
+		funds[groupAddress].contributors.push(msg.sender);
+		contributionTracker[groupAddress][msg.sender] = true;
+
+		emit NewFundContribution(groupAddress, msg.sender, msg.value);
+	}
+
+	/**
+	 * @notice Cancel a fundraise and return funds to contributors
+	 * @param groupAddress Address of group
+	 */
+	function cancelFundProposal(address groupAddress) public virtual nonReentrant {
+		if (IForumGroup(groupAddress).balanceOf(msg.sender, MEMBERSHIP) == 0) revert NotMember();
+
+		Fund storage fund = funds[groupAddress];
+
+		// Only groupAddress or proposer can cancel the fundraise.
+		if (!(msg.sender == groupAddress || msg.sender == fund.contributors[0])) revert NotProposer();
+
+		// Return funds from escrow
+		for (uint256 i; i < fund.contributors.length; ) {
+			payable(fund.contributors[i]).transfer(fund.individualContribution);
+			contributionTracker[groupAddress][fund.contributors[i]] = false;
+
+			// Members can only be 12
+			unchecked {
+				++i;
+			}
+		}
+
+		delete funds[groupAddress];
+
+		emit FundProposalCancelled(groupAddress);
+	}
+
+	/**
+	 * @notice Process the fundraise, sending AVAX to group and minting tokens to contributors
+	 * @param groupAddress Address of group
+	 */
+	function processFundProposal(address groupAddress) public virtual nonReentrant {
+		Fund storage fund = funds[groupAddress];
+
+		if (funds[groupAddress].individualContribution == 0) revert FundraiseMissing();
+
+		uint256 memberCount = fund.contributors.length;
+
+		// We adjust the number of shares distributed based on the unitValue of group tokens
+		// This ensures that members get a fair number of tokens given the value of the treasury at any time
+		uint256 adjustedContribution = (fund.individualContribution * fund.valueDenominator) /
+			fund.valueNumerator;
+
+		if (memberCount != IForumGroup(groupAddress).memberCount()) revert MembersMissing();
+
+		groupAddress._safeTransferETH(fund.individualContribution * memberCount);
+
+		for (uint256 i; i < memberCount; ) {
+			// Mint member an share of tokens equal to their contribution and reset their status in the tracker
+			IForumGroup(groupAddress).mintShares(fund.contributors[i], TOKEN, adjustedContribution);
+			contributionTracker[groupAddress][fund.contributors[i]] = false;
+
+			// Members can only be 12
+			unchecked {
+				++i;
+			}
+		}
+
+		delete funds[groupAddress];
+
+		emit FundProposalReleased(groupAddress, fund.contributors, fund.individualContribution);
+	}
+
+	/**
+	 * @notice Get the details of a fundraise
+	 * @param groupAddress Address of group
+	 * @return contributors Array of contributors
+	 * @return individualContribution Individual contribution of each contributor
+	 */
+	function getFund(address groupAddress)
+		public
+		view
+		returns (address[] memory contributors, uint256 individualContribution)
+	{
+		Fund storage fund = funds[groupAddress];
+
+		contributors = fund.contributors;
+		individualContribution = fund.individualContribution;
+	}
+}
+
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.13;
+
+/// @notice Safe ETH and ERC-20 transfer library that gracefully handles missing return values.
+/// @author Modified from KaliDAO (https://github.com/kalidao/kali-contracts/blob/main/contracts/libraries/SafeTransferLib.sol)
+/// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer.
+library SafeTransferLib {
+	/*///////////////////////////////////////////////////////////////
+                            ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+	error ETHtransferFailed();
+
+	error TransferFailed();
+
+	error TransferFromFailed();
+
+	/*///////////////////////////////////////////////////////////////
+                            ETH OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+	function _safeTransferETH(address to, uint256 amount) internal {
+		bool callStatus;
+
+		assembly {
+			// transfer the ETH and store if it succeeded or not
+			callStatus := call(gas(), to, amount, 0, 0, 0, 0)
+		}
+
+		if (!callStatus) revert ETHtransferFailed();
+	}
+
+	/*///////////////////////////////////////////////////////////////
+                            ERC20 OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+	function _safeTransfer(
+		address token,
+		address to,
+		uint256 amount
+	) internal {
+		bool callStatus;
+
+		assembly {
+			// get a pointer to some free memory
+			let freeMemoryPointer := mload(0x40)
+
+			// write the abi-encoded calldata to memory piece by piece:
+			mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+
+			mstore(add(freeMemoryPointer, 4), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+
+			mstore(add(freeMemoryPointer, 36), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
+
+			// call the token and store if it succeeded or not
+			// we use 68 because the calldata length is 4 + 32 * 2
+			callStatus := call(gas(), token, 0, freeMemoryPointer, 68, 0, 0)
+		}
+
+		if (!_didLastOptionalReturnCallSucceed(callStatus)) revert TransferFailed();
+	}
+
+	function _safeTransferFrom(
+		address token,
+		address from,
+		address to,
+		uint256 amount
+	) internal {
+		bool callStatus;
+
+		assembly {
+			// get a pointer to some free memory
+			let freeMemoryPointer := mload(0x40)
+
+			// write the abi-encoded calldata to memory piece by piece:
+			mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000) // begin with the function selector
+
+			mstore(add(freeMemoryPointer, 4), and(from, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "from" argument
+
+			mstore(add(freeMemoryPointer, 36), and(to, 0xffffffffffffffffffffffffffffffffffffffff)) // mask and append the "to" argument
+
+			mstore(add(freeMemoryPointer, 68), amount) // finally append the "amount" argument - no mask as it's a full 32 byte value
+
+			// call the token and store if it succeeded or not
+			// we use 100 because the calldata length is 4 + 32 * 3
+			callStatus := call(gas(), token, 0, freeMemoryPointer, 100, 0, 0)
+		}
+
+		if (!_didLastOptionalReturnCallSucceed(callStatus)) revert TransferFromFailed();
+	}
+
+	/*///////////////////////////////////////////////////////////////
+                            INTERNAL HELPER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+	function _didLastOptionalReturnCallSucceed(bool callStatus) internal pure returns (bool success) {
+		assembly {
+			// get how many bytes the call returned
+			let returnDataSize := returndatasize()
+
+			// if the call reverted:
+			if iszero(callStatus) {
+				// copy the revert message into memory
+				returndatacopy(0, 0, returnDataSize)
+
+				// revert with the same message
+				revert(0, returnDataSize)
+			}
+
+			switch returnDataSize
+			case 32 {
+				// copy the return data into memory
+				returndatacopy(0, 0, returnDataSize)
+
+				// set success to whether it returned true
+				success := iszero(iszero(mload(0)))
+			}
+			case 0 {
+				// there was no return data
+				success := 1
+			}
+			default {
+				// it returned some malformed input
+				success := 0
+			}
+		}
+	}
+}
+
+// SPDX-License-Identifier: AGPL-3.0-only
+// pragma solidity ^0.8.13;
+
+/// @notice Gas optimized reentrancy protection for smart contracts
+/// @author Modified from KaliDAO (https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/security/ReentrancyGuard.sol)
+abstract contract ReentrancyGuard {
+	error Reentrancy();
+
+	uint256 private constant _NOT_ENTERED = 1;
+	uint256 private constant _ENTERED = 2;
+
+	uint256 private _status;
+
+	constructor() {
+		_status = _NOT_ENTERED;
+	}
+
+	modifier nonReentrant() {
+		// On the first call to nonReentrant, _notEntered will be true
+		//require(_status != _ENTERED, 'ReentrancyGuard: reentrant call');
+		if (_status == _ENTERED) revert Reentrancy();
+
+		// Any calls to nonReentrant after this point will fail
+		_status = _ENTERED;
+
+		_;
+
+		// By storing the original value once again, a refund is triggered (see
+		// https://eips.ethereum.org/EIPS/eip-2200)
+		_status = _NOT_ENTERED;
+	}
+}
+
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity ^0.8.13;
+
+/// @notice ForumGroup interface
+interface IForumGroup {
+	function balanceOf(address to, uint256 tokenId) external payable returns (uint256);
+
+	function proposalCount() external payable returns (uint256);
+
+	function memberCount() external payable returns (uint256);
+
+	function mintShares(
+		address to,
+		uint256 id,
+		uint256 amount
+	) external payable;
+
+	function burnShares(
+		address from,
+		uint256 id,
+		uint256 amount
+	) external payable;
+}
