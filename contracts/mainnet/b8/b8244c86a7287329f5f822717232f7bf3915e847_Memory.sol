9a5064fd84ddf7b9fc294a42cee589d8ae1de32d
minion <minion>
2022-08-27 11:03:21 +0000

    with 💕

---
+++ contracts/mainnet/b8/b8244c86a7287329f5f822717232f7bf3915e847_Memory.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at snowtrace.io on 2022-08-27
+*/
+
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.4;
+
+contract Memory {
+    function castMemory(
+        bytes32 id, bytes32 memory_name, uint amount, uint timestamp,
+        bytes32 assetNameHash
+    ) public {}
+}
