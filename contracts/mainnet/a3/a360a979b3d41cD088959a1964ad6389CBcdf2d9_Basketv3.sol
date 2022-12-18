b173172d75b2e554885310c4f18b400937fc6327
minion <minion>
2022-12-18 17:06:32 +0000

    with 💕

---
+++ contracts/mainnet/a3/a360a979b3d41cD088959a1964ad6389CBcdf2d9_Basketv3.sol
@@ -0,0 +1,378 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "./../interface/IUniswapRouterETH.sol";
+import "./../interface/IWETH.sol";
+
+import "./../interface/stargate/IStargateRouter.sol";
+
+contract Basketv3 is Ownable {
+  mapping (address => bool) public managers;
+
+  address public stargaterouter;
+  address public unirouter;
+  address[] public nativeToStargateInput;
+  address[] public stargateInputToNative;
+  address public native;
+  address public stargateInput;
+  uint256 public stargateSourcePoolId;
+  uint256 public stargateSwapFeeMultipler = 1400000;
+  uint256 public stargateSwapFeeDivider = 1000000;
+
+  struct StragateSwapInfo {
+    uint16 chain;
+    address basketAddress;
+    uint256 srcPoolID;
+    uint256 dstPoolID;
+    uint256 poolCnts;
+  }
+
+  constructor(
+    address _unirouter,
+    address _stargaterouter,
+    uint256 _stargateSourcePoolId,
+    address[] memory _nativeToStargateInput,
+    address[] memory _stargateInputToNative
+  ) {
+    managers[msg.sender] = true;
+    stargaterouter = _stargaterouter;
+    unirouter = _unirouter;
+    nativeToStargateInput = _nativeToStargateInput;
+    stargateInputToNative = _stargateInputToNative;
+    native = _nativeToStargateInput[0];
+    stargateInput = _nativeToStargateInput[_nativeToStargateInput.length - 1];
+    stargateSourcePoolId = _stargateSourcePoolId;
+
+    _approveTokenIfNeeded(native, unirouter);
+    _approveTokenIfNeeded(stargateInput, unirouter);
+    _approveTokenIfNeeded(stargateInput, stargaterouter);
+  }
+
+  receive() external payable {
+  }
+
+  function deposit(StragateSwapInfo calldata _swap) public payable {
+    (uint256 swFee, ) = IStargateRouter(stargaterouter).quoteLayerZeroFee(
+      _swap.chain,
+      1,
+      bytes("0x"),
+      bytes("0x"),
+      IStargateRouter.lzTxObj(0, 0, "0x")
+    );
+    swFee = swFee * stargateSwapFeeMultipler / stargateSwapFeeDivider;
+
+    IWETH(native).deposit{value: msg.value-swFee}();
+    uint256[] memory amounts = IUniswapRouterETH(unirouter).swapExactTokensForTokens(msg.value-swFee, 0, nativeToStargateInput, address(this), block.timestamp);
+    _stgBridgeSwap(_swap, swFee, amounts[amounts.length - 1]);
+
+    if (address(this).balance > 0) {
+      (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+      require(success, "Failed refund change");
+    }
+  }
+
+  function _stgBridgeSwap(StragateSwapInfo calldata _swap, uint256 _swfee, uint256 _iamount) internal {
+    IStargateRouter(stargaterouter).swap{value: _swfee}(
+      _swap.chain,
+      stargateSourcePoolId,
+      _swap.dstPoolID,
+      payable(address(this)),
+      _iamount,
+      0,
+      IStargateRouter.lzTxObj(0, 0, "0x"),
+      abi.encodePacked(_swap.basketAddress),
+      bytes("")
+    );
+  }
+
+  function withdrawBridgeRefundFee(uint256 _fee) public onlyOwner {
+    (bool success, ) = payable(msg.sender).call{value: _fee}("");
+    require(success, "Failed to withdraw");
+  }
+
+  function _approveTokenIfNeeded(address token, address spender) private {
+    if (IERC20(token).allowance(address(this), spender) == 0) {
+      IERC20(token).approve(spender, type(uint256).max);
+    }
+  }
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.9 <0.9.0;
+
+interface IStargateRouter {
+    struct lzTxObj {
+        uint256 dstGasForCall;
+        uint256 dstNativeAmount;
+        bytes dstNativeAddr;
+    }
+
+    function addLiquidity(uint256 _poolId, uint256 _amountLD, address to) external payable;
+    function instantRedeemLocal(uint16 _srcPoolId, uint256 _amountLD, address to) external payable;
+
+    function swap(
+        uint16 _dstChainId,
+        uint256 _srcPoolId,
+        uint256 _dstPoolId,
+        address payable _refundAddress,
+        uint256 _amountLD,
+        uint256 _minAmountLD,
+        lzTxObj memory _lzTxParams,
+        bytes calldata _to,
+        bytes calldata _payload
+    ) external payable;
+
+    function quoteLayerZeroFee(
+        uint16 _dstChainId,
+        uint8 _functionType,
+        bytes calldata _toAddress,
+        bytes calldata _transferAndCallPayload,
+        lzTxObj memory _lzTxParams
+    ) external view returns (uint256, uint256);
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.9 <0.9.0;
+
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function withdraw(uint256 wad) external;
+}
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+interface IUniswapRouterETH {
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        payable
+        returns (uint[] memory amounts);
+    
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        returns (uint[] memory amounts);
+
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+
+    function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
+    function getAmountsIn(uint amountOut, address[] calldata path) external view returns (uint[] memory amounts);
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.6.0) (token/ERC20/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (interfaces/IERC20.sol)
+
+pragma solidity ^0.8.0;
+
+import "../token/ERC20/IERC20.sol";
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
+
+pragma solidity ^0.8.0;
+
+import "../utils/Context.sol";
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
