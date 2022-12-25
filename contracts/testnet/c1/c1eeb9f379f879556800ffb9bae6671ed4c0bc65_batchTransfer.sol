d67d9f2ad8b153dd8df121e7d71a2031f112cb49
minion <minion>
2022-12-25 07:03:11 +0000

    with 💕

---
+++ contracts/testnet/c1/c1eeb9f379f879556800ffb9bae6671ed4c0bc65_batchTransfer.sol
@@ -0,0 +1,187 @@
+/**
+ *Submitted for verification at testnet.snowtrace.io on 2022-12-24
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+ 
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+
+interface IAVAX20 {
+    function totalSupply() external view returns (uint256);
+    function deposit(uint256 amount) external payable;
+    function balanceOf(address who) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+    external view returns (uint256);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function approve(address spender, uint256 value)
+    external returns (bool);
+
+    function transferFrom(address from, address to, uint256 value)
+    external returns (bool);
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+}
+
+interface IWAVAX {
+    function deposit() external payable;
+    function transfer(address to, uint value) external returns (bool);
+    function withdraw(uint) external;
+    function balanceOf(address who) external view returns (uint256);
+}
+
+
+library SafeMath {
+    function add(uint x, uint y) internal pure returns (uint z) {
+        require((z = x + y) >= x, 'ds-math-add-overflow');
+    }
+
+    function sub(uint x, uint y) internal pure returns (uint z) {
+        require((z = x - y) <= x, 'ds-math-sub-underflow');
+    }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
+}
+
+contract batchTransfer is Ownable{
+    
+    using SafeMath for uint;
+    
+    
+    
+
+    fallback() external payable{
+          
+    }
+    
+    function transfer(address[] memory addresses, uint256 amount) external onlyOwner() {
+        
+        for(uint256 i=0; i<addresses.length; i++){
+            payable(addresses[i]).transfer(amount);
+        }
+    
+    }
+    
+    
+   
+       
+    event Received(address, uint);
+    receive() external payable {
+        emit Received(msg.sender, msg.value);
+    }
+    
+    function withdrawAVAX() external onlyOwner() {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+    
+    function withdrawToken(uint256 amount, address token) external onlyOwner{
+         IAVAX20(token).transfer(msg.sender, amount);
+    }
+
+    function getBalanceOfToken(address _address) public view returns (uint256) {
+        return IAVAX20(_address).balanceOf(address(this));
+    }
+
+    //function updateBalanceOfWAVAX() public view returns (uint256) {
+        //return IAVAX20(_address).balanceOf(address(this));
+    //}
+    
+    
+    // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    function calculate(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
+        uint amountInWithFee = amountIn.mul(997);
+        uint numerator = amountInWithFee.mul(reserveOut);
+        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        amountOut = numerator / denominator;
+    }
+
+    
+     // performs chained getAmountIn calculations on any number of pairs
+    function getAmountOut(address[] memory path , address pair,  uint256 amount) internal view returns (uint amountOut) {
+        
+        
+    }
+
+}
