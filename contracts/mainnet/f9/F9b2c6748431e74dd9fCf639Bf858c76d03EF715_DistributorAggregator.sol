d842a66193d22b2b301d1c7f39562c84f5e1da27
minion <minion>
2023-02-15 11:02:39 +0000

    with 💕

---
+++ contracts/mainnet/f9/F9b2c6748431e74dd9fCf639Bf858c76d03EF715_DistributorAggregator.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.12;
+
+import "./IDistributor.sol";
+
+contract DistributorAggregator {
+    // Withdrawal
+    function withdraw(address[] calldata distributorList) external {
+        for(uint16 i=0; i < distributorList.length; i++) {
+            IDistributor distributor = IDistributor(distributorList[i]);
+            distributor.computeCumulativeShare(msg.sender);
+            uint40 share = distributor.cumulativeShareOf(msg.sender);
+            if (share > 0) {
+                distributor.withdrawTo(msg.sender);
+            }
+        }
+    }
+}
+
+// SPDX-License-Identifier: MIT
+interface IDistributor {
+  function addIncome ( uint40 amount ) external;
+  function computeCumulativeShare ( address tokenHolder ) external;
+  function cumulativeShareOf ( address tokenHolder ) external view returns ( uint40 );
+  function owner (  ) external view returns ( address );
+  function renounceOwnership (  ) external;
+  function transferOwnership ( address newOwner ) external;
+  function transferToOwner ( uint256 amount ) external;
+  function withdraw (  ) external;
+  function withdrawTo(address tokenHolder) external;
+}
