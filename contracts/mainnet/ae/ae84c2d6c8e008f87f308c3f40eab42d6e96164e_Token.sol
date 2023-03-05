433461f5b7dfb4a215f0cfabad470341d8fe70c5
minion <minion>
2023-03-05 21:02:29 +0000

    with 💕

---
+++ contracts/mainnet/ae/ae84c2d6c8e008f87f308c3f40eab42d6e96164e_Token.sol
@@ -0,0 +1,51 @@
+/**
+ *Submitted for verification at snowtrace.io on 2023-03-05
+*/
+
+pragma solidity ^0.8.7;
+
+contract Token {
+    mapping(address => uint) public balances;
+    mapping(address => mapping(address => uint)) public allowance;
+    uint public totalSupply = 21000000 * 10 ** 18;
+    string public name = "LATZ Coin";
+    string public symbol = "LTZ";
+    uint public decimals = 18;
+    
+    event Transfer(address indexed from, address indexed to, uint value);
+    event Approval(address indexed owner, address indexed spender, uint value);
+    
+    constructor() {
+        balances[msg.sender] = totalSupply;
+    }
+    
+    function balanceOf(address owner) public view returns (uint) {
+        return balances[owner];
+    }
+    
+    function transfer(address to, uint value) public returns (bool) {
+        require(to != address(0), "Invalid address");
+        require(balanceOf(msg.sender) >= value, "Balance too low");
+        balances[to] += value;
+        balances[msg.sender] -= value;
+        emit Transfer(msg.sender, to, value);
+        return true;
+    }
+    
+    function transferFrom(address from, address to, uint value) public returns (bool) {
+        require(from != address(0), "Invalid sender address");
+        require(to != address(0), "Invalid recipient address");
+        require(balanceOf(from) >= value, "Balance too low");
+        require(allowance[from][msg.sender] >= value, "Allowance too low");
+        balances[to] += value;
+        balances[from] -= value;
+        emit Transfer(from, to, value);
+        return true;
+    }
+    
+    function approve(address spender, uint value) public returns (bool) {
+        allowance[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+}
