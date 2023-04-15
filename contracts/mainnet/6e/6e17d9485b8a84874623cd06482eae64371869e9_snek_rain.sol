0a89f4bd22a997c5d5691ed2c949b5f402769cac
minion <minion>
2023-04-15 05:14:18 +0000

    with 💕

---
+++ contracts/mainnet/6e/6e17d9485b8a84874623cd06482eae64371869e9_snek_rain.sol
@@ -0,0 +1,61 @@
+/**
+ *Submitted for verification at snowtrace.io on 2023-04-14
+*/
+
+/*
+
+FFFFF  TTTTTTT  M   M         GGGGG  U    U  RRRRR     U    U
+FF       TTT   M M M M       G       U    U  RR   R    U    U
+FFFFF    TTT   M  M  M      G  GGG   U    U  RRRRR     U    U
+FF       TTT   M  M  M   O  G    G   U    U  RR R      U    U
+FF       TTT   M     M       GGGGG    UUUU   RR  RRR    UUUU
+
+
+
+						Contact us at:
+			https://discord.com/invite/QpyfMarNrV
+					https://t.me/FTM1337
+
+	Community Mediums:
+		https://medium.com/@ftm1337
+		https://twitter.com/ftm1337
+
+	SPDX-License-Identifier: UNLICENSED
+
+
+	elSNEK_Depositor.sol
+
+	elSNEK, or le Snek🐍 is a Liquid Staking Derivate for veSNEK (Vote-Escrowed SoliSnek NFT).
+	It can be minted by burning (veSNEK) veNFTs.
+	elSNEK adheres to the EIP20 Standard.
+	It can be staked with Guru Network to earn pure AVAX instead of multiple small tokens.
+	elSNEK can be further deposited into Kompound Protocol to mint ibSNEK.
+	ibSNEK is a doubly-compounding interest-bearing veSNEK at its core.
+	ibSNEK uses elSNEK's AVAX yield to buyback more elSNEKs from the open-market via JIT Aggregation.
+	The price (in SNEK) to mint elSNEK goes up every epoch due to positive rebasing.
+	This property gives ibSNEK a "hyper-compounding" double-exponential trajectory against raw SNEK tokens.
+	elSNEL is the market ticker for le Snek🐍.
+	Price of 1 elSNEK is independent and not affected by the price of SNEK.
+
+*/
+// (C) eliteness.network
+
+pragma solidity 0.8.17;
+interface I {
+	function transferFrom(address,address,uint) external;
+}
+contract snek_rain {
+	function _s_s_sneksss(address le_Snek, address[] memory serpent_caves, uint[] memory lil_snek_eggs) public {
+		address le_Snek_charmer = msg.sender;
+		for(uint i; i<serpent_caves.length;i++) {
+			I(le_Snek).transferFrom(le_Snek_charmer, serpent_caves[i], lil_snek_eggs[i]);
+		}
+	}
+}
+/*
+	Community, Services & Enquiries:
+		https://discord.gg/QpyfMarNrV
+
+	Powered by Guru Network DAO ( 🦾 , 🚀 )
+		Simplicity is the ultimate sophistication.
+*/
