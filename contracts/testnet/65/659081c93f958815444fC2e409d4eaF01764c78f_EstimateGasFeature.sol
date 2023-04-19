f12d495fd52ad9a16399d5d92645454fa87401ed
minion <minion>
2023-04-19 07:21:37 +0000

    with 💕

---
+++ contracts/testnet/65/659081c93f958815444fC2e409d4eaF01764c78f_EstimateGasFeature.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+
+contract EstimateGasFeature {
+
+    struct ERC20Pair {
+        address token;
+        uint256 amount;
+    }
+
+    function estimateGasBatchBuyWithETH(bytes calldata tradeBytes) external payable returns(uint256) {
+        assembly {
+            mstore(0, 0x4c674c2d)
+            calldatacopy(0x20, 0x4, sub(calldatasize(), 0x4))
+            if delegatecall(gas(), address(), 0x4, calldatasize(), 0, 0) {
+                mstore(0, gas())
+                return(0, 0x20)
+            }
+            returndatacopy(0, 0, returndatasize())
+            revert(0, returndatasize())
+        }
+    }
+
+    function estimateGasBatchBuyWithERC20s(
+        ERC20Pair[] calldata erc20Pairs,
+        bytes calldata tradeBytes,
+        address[] calldata dustTokens
+    ) external payable returns(uint256) {
+        assembly {
+            mstore(0, 0x5d578816)
+            calldatacopy(0x20, 0x4, sub(calldatasize(), 0x4))
+            if delegatecall(gas(), address(), 0x4, calldatasize(), 0, 0) {
+                mstore(0, gas())
+                return(0, 0x20)
+            }
+            returndatacopy(0, 0, returndatasize())
+            revert(0, returndatasize())
+        }
+    }
+}
