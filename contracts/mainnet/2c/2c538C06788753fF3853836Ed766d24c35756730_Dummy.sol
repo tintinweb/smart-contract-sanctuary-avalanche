79516f2002c7e00041bb8e8829b3e87fa91e68ce
minion <minion>
2023-06-14 21:05:20 +0000

    with 💕

---
+++ contracts/mainnet/2c/2c538C06788753fF3853836Ed766d24c35756730_Dummy.sol
@@ -0,0 +1,426 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.14;
+
+import "ERC2771Context.sol";
+
+import "ContextMixin.sol";
+import "NativeMetaTransaction.sol";
+
+contract Dummy {
+    address public user;
+    address public relay;
+    address public addr;
+
+    function changeAddress(address _address) public virtual {
+        addr = _address;
+        user = msg.sender;
+        relay = msg.sender;
+    }
+}
+
+contract DummyNative is Dummy, NativeMetaTransaction, ContextMixin {
+    string public constant CONTRACT_NAME = "DummyNative";
+    string public constant EIP712_VERSION = "1";
+
+    constructor () {
+        _setDomainSeparator(CONTRACT_NAME, EIP712_VERSION);
+    }
+
+    function changeAddress(address _address) public override {
+        addr = _address;
+        user = _msgSender();
+        relay = msg.sender;
+    }
+}
+
+contract Dummy2771 is Dummy, ERC2771Context {
+    constructor(address trustedForwarder) ERC2771Context(trustedForwarder) {
+    }
+
+    function changeAddress(address _address) public override {
+        addr = _address;
+        user = _msgSender();
+        relay = msg.sender;
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.5.0) (metatx/ERC2771Context.sol)
+
+pragma solidity ^0.8.9;
+
+import "Context.sol";
+
+/**
+ * @dev Context variant with ERC2771 support.
+ */
+abstract contract ERC2771Context is Context {
+    /// @custom:oz-upgrades-unsafe-allow state-variable-immutable
+    address private immutable _trustedForwarder;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor(address trustedForwarder) {
+        _trustedForwarder = trustedForwarder;
+    }
+
+    function isTrustedForwarder(address forwarder) public view virtual returns (bool) {
+        return forwarder == _trustedForwarder;
+    }
+
+    function _msgSender() internal view virtual override returns (address sender) {
+        if (isTrustedForwarder(msg.sender)) {
+            // The assembly code is more direct than the Solidity version using `abi.decode`.
+            assembly {
+                sender := shr(96, calldataload(sub(calldatasize(), 20)))
+            }
+        } else {
+            return super._msgSender();
+        }
+    }
+
+    function _msgData() internal view virtual override returns (bytes calldata) {
+        if (isTrustedForwarder(msg.sender)) {
+            return msg.data[:msg.data.length - 20];
+        } else {
+            return super._msgData();
+        }
+    }
+}
+
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (utils/Context.sol)
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.14;
+
+abstract contract ContextMixin {
+    function _msgSender()
+        internal
+        view
+        returns (address sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.14;
+
+import "EIP712Domain.sol";
+import "Nonces.sol";
+
+abstract contract NativeMetaTransaction is EIP712Domain, Nonces {
+    // keccak256("MetaTransaction(uint256 nonce,address from,bytes functionSignature)")
+    bytes32
+        public constant META_TRANSACTION_TYPEHASH = 0x23d10def3caacba2e4042e0c75d44a42d2558aabcf5ce951d0642a8032e1e653;
+
+    event MetaTransactionExecuted(
+        address userAddress,
+        address relayerAddress,
+        bytes functionSignature
+    );
+
+    /*
+     * Meta transaction structure.
+     * No point of including value field here as if user is doing value transfer then he has the funds to pay for gas
+     * He should call the desired function directly in that case.
+     */
+    struct MetaTransaction {
+        uint256 nonce;
+        address from;
+        bytes functionSignature;
+    }
+
+    function executeMetaTransaction(
+        address userAddress,
+        bytes memory functionSignature,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    ) external payable returns (bytes memory) {
+        MetaTransaction memory metaTx = MetaTransaction({
+            nonce: _nonces[userAddress]++,
+            from: userAddress,
+            functionSignature: functionSignature
+        });
+
+        require(
+            _verify(userAddress, metaTx, sigR, sigS, sigV),
+            "Signer and signature do not match"
+        );
+
+        emit MetaTransactionExecuted(
+            userAddress,
+            msg.sender,
+            functionSignature
+        );
+
+        // Append userAddress and relayer address at the end to extract it from calling context
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returnData) = address(this).call(
+            abi.encodePacked(functionSignature, userAddress)
+        );
+        require(success, "Function call not successful");
+
+        return returnData;
+    }
+
+    function _verify(
+        address signer,
+        MetaTransaction memory metaTx,
+        bytes32 sigR,
+        bytes32 sigS,
+        uint8 sigV
+    ) internal view returns (bool) {
+        require(signer != address(0), "NativeMetaTransaction: INVALID_SIGNER");
+
+        bytes memory data = abi.encode(
+            META_TRANSACTION_TYPEHASH,
+            metaTx.nonce,
+            metaTx.from,
+            keccak256(metaTx.functionSignature)
+        );
+
+        return
+            EIP712.recover(DOMAIN_SEPARATOR, sigV, sigR, sigS, data) == signer;
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.14;
+
+import "EIP712.sol";
+
+abstract contract EIP712Domain {
+    bytes32 public DOMAIN_SEPARATOR;
+
+    function _setDomainSeparator(string memory name, string memory version)
+        internal
+    {
+        DOMAIN_SEPARATOR = EIP712.makeDomainSeparator(name, version);
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * Copyright (c) 2018-2020 CENTRE SECZ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+pragma solidity 0.8.14;
+
+import "ECRecover.sol";
+
+/**
+ * @title EIP712
+ * @notice A library that provides EIP712 helper functions
+ */
+library EIP712 {
+    // keccak256("EIP712Domain(string name,string version,address verifyingContract,bytes32 salt)")
+    bytes32
+        public constant EIP712_DOMAIN_TYPEHASH = 0x36c25de3e541d5d970f66e4210d728721220fff5c077cc6cd008b3a0c62adab7;
+
+    /**
+     * @notice Make EIP712 domain separator
+     * @param name      Contract name
+     * @param version   Contract version
+     * @return Domain separator
+     */
+    function makeDomainSeparator(string memory name, string memory version)
+        internal
+        view
+        returns (bytes32)
+    {
+        uint256 chainId;
+        assembly {
+            chainId := chainid()
+        }
+
+        return
+            keccak256(
+                abi.encode(
+                    EIP712_DOMAIN_TYPEHASH,
+                    keccak256(bytes(name)),
+                    keccak256(bytes(version)),
+                    address(this),
+                    bytes32(chainId)
+                )
+            );
+    }
+
+    /**
+     * @notice Recover signer's address from a EIP712 signature
+     * @param domainSeparator   Domain separator
+     * @param v                 v of the signature
+     * @param r                 r of the signature
+     * @param s                 s of the signature
+     * @param typeHashAndData   Type hash concatenated with data
+     * @return Signer's address
+     */
+    function recover(
+        bytes32 domainSeparator,
+        uint8 v,
+        bytes32 r,
+        bytes32 s,
+        bytes memory typeHashAndData
+    ) internal pure returns (address) {
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(typeHashAndData)
+            )
+        );
+        return ECRecover.recover(digest, v, r, s);
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+/**
+ * Copyright (c) 2016-2019 zOS Global Limited
+ * Copyright (c) 2018-2020 CENTRE SECZ
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+pragma solidity 0.8.14;
+
+/**
+ * @title ECRecover
+ * @notice A library that provides a safe ECDSA recovery function
+ */
+library ECRecover {
+    /**
+     * @notice Recover signer's address from a signed message
+     * @dev Adapted from: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/65e4ffde586ec89af3b7e9140bdc9235d1254853/contracts/cryptography/ECDSA.sol
+     * Modifications: Accept v, r, and s as separate arguments
+     * @param digest    Keccak-256 hash digest of the signed message
+     * @param v         v of the signature
+     * @param r         r of the signature
+     * @param s         s of the signature
+     * @return Signer address
+     */
+    function recover(
+        bytes32 digest,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) internal pure returns (address) {
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        if (
+            uint256(s) >
+            0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0
+        ) {
+            revert("ECRecover: invalid signature 's' value");
+        }
+
+        if (v != 27 && v != 28) {
+            revert("ECRecover: invalid signature 'v' value");
+        }
+
+        // If the signature is valid (and not malleable), return the signer address
+        address signer = ecrecover(digest, v, r, s);
+        require(signer != address(0), "ECRecover: invalid signature");
+
+        return signer;
+    }
+}
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.8.14;
+
+abstract contract Nonces {
+    mapping(address => uint256) internal _nonces;
+
+    /**
+     * @notice Nonces for permit / meta-transactions
+     * @param owner Token owner's address
+     * @return Next nonce
+     */
+    function nonces(address owner) external view returns (uint256) {
+        return _nonces[owner];
+    }
+}
