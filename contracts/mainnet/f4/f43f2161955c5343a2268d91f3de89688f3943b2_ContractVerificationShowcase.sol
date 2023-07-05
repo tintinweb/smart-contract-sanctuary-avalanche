9bc16b9024953637510204332478d2bcc3c77ce6
minion <minion>
2023-07-05 17:04:59 +0000

    with 💕

---
+++ contracts/mainnet/f4/f43f2161955c5343a2268d91f3de89688f3943b2_ContractVerificationShowcase.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.19;
+
+contract ContractVerificationShowcase {
+
+    function isVerified() external view returns (bool) {
+        return true;
+    }
+}
