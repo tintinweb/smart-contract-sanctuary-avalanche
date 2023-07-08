e3c7116de535253d2ab24517ec447b9a0d32ab7b
minion <minion>
2023-07-08 17:04:18 +0000

    with 💕

---
+++ contracts/mainnet/88/8827576582c4ad242edaa0a15c5f43ea83faa061_DoNoting.sol
@@ -0,0 +1,13 @@
+/**
+ *Submitted for verification at snowtrace.io on 2023-07-08
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+
+contract DoNoting{
+   
+    function nothing () public pure{ // 183 gas
+              1+1; 
+        }
+}
